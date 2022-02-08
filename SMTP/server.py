3a28c87d0540decdb0c6f6be863d498dffb210e1
Under-estimate <1260717118@qq.com>
2022-02-08 18:18:32 +0800

    SMTP: Approach Standard

--- SMTP/server.py
+++ SMTP/server.py
@@ -3,6 +3,8 @@ import socketserver
 import pymysql
 import base64
 import hashlib
+import time
+import re
 import cryptography
 
 db = pymysql.connect(host='localhost',
@@ -14,8 +16,14 @@ cursor = db.cursor()
 sql_query = "SELECT * FROM users WHERE username = %s"
 sql_save = "INSERT INTO MAILS(uid,sender,receiver,create_time,content) VALUES (UUID(), %s, %s, now(), %s)"
 
+re_email = re.compile(r'^[a-zA-Z\.]+@[a-zA-Z0-9]+\.[a-zA-Z]{3}$')
 
 class Myserver(socketserver.BaseRequestHandler):
+
+    def get_time(self):
+        ticks = time.time()
+        localtime = time.localtime(time.time())
+        return time.strftime("%a ,%d %b %Y %H:%M:%S %z", time.localtime())
 
     def recv_endswith(self, End):
         total_data = []
@@ -38,19 +46,30 @@ class Myserver(socketserver.BaseRequestHandler):
     def check_ip_list(self):  # 获取域名解析出的IP列表
         ip_list = []
         self.domain.encode("utf-8")
+        if len(self.domain) <= 1:
+            self.request.sendall(b'502 please enter domain\r\n')
+            return
+
         try:
             addrs = socket.getaddrinfo(self.domain, None)
-
             for item in addrs:
                 if item[4][0] not in ip_list:
                     ip_list.append(item[4][0])
         except Exception as e:
             print(str(e))
-            pass
+            self.request.sendall(b'502 Invalid domain,getaddrinfo failed\r\n')
+            return
+
+        self.request.sendall(b'''250-msc.com
+250-SIZE 73400320
+250-AUTH LOGIN 
+250-AUTH=LOGIN
+250 8BITMIME\r\n.\r\n''')
 
         for ip in ip_list:
             if ip == self.client_ip:
                 self.client_type = 1
+                return
 
     def check_existence(self, user_addr):
         if user_addr.endswith(self.server_domain):
@@ -67,7 +86,10 @@ class Myserver(socketserver.BaseRequestHandler):
                 pass
             return False
         else:
-            return True
+            if re_email.match(user_addr):
+                return True
+            else:
+                return False
 
     def check_user(self):
         data_tuple = (self.client_name,)
@@ -82,83 +104,135 @@ class Myserver(socketserver.BaseRequestHandler):
             print(str(e))
             pass
 
-    def save_mail(self, receiver_addr):
-        data_tuple=(self.sender_addr, receiver_addr, self.content)
+    def create_mail(self, sender, cont):
+        From = "From: " + self.server_user + "\n"
+        To = "To: " + sender + "\n"
+        Date = "Date: " + self.get_time() + "\n"
+        Subject = "Subject: =?utf-8?b?5a+55LiN6LW377yM5oKo55qE6YKu5Lu25Y+R6YCB5aSx6LSl?=\n"
+        Setting = "Content-Type: text/plain; charset=utf-8\n"
+        return From + To + Date + Subject + Setting + cont
+
+    def save_mail(self, sender, rec, cont):
+        cont = "Received : from " + self.domain \
+               + " by " + self.server_domain \
+               + " for <" + rec \
+               + "> ; " + self.get_time() \
+               + "\n" + cont
+        data_tuple = (sender, rec, cont)
         try:
-            cursor.execute(sql_save,data_tuple)
+            cursor.execute(sql_save, data_tuple)
             db.commit()
-            self.request.sendall(b'250 queued as\r\n')
         except Exception as e:
             print(str(e))
             # 如果发生错误则回滚
-            self.request.sendall(b'502 unable to fetch data\r\n')
             db.rollback()
             pass
 
-    def send_mail(self, receiver_addr):
+    def send_mail(self, sender, rec, cont):
+        cont = "Received : from " + self.domain \
+               + " by " + self.server_domain \
+               + " for <" + rec \
+               + "> ; " + self.get_time() \
+               + "\n" + cont
+
         tcpclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcpclient.connect((self.client_ip, 25))
-        ret = tcpclient.recv(1024)
+        ret = self.recv_endswith("\r\n").strip()
 
-        data = "EHLO " + self.server_domain + "\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = "EHLO " + self.server_domain + "\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n.\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
-        data = "MAIL FROM:<" + self.sender_addr + ">\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = "MAIL FROM:<" + sender + ">\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
-        data = "RCPT TO:<" + receiver_addr + ">\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = "RCPT TO:<" + rec + ">\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
-        data = "DATA\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = "DATA\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
-        data = self.content + "\r\n.\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = cont + "\r\n.\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
-        data = "QUIT\r\n"
-        tcpclient.send(data.encode('utf-8'))
-        ret = tcpclient.recv(1024)
+        dat = "QUIT\r\n"
+        tcpclient.send(dat.encode('utf-8'))
+        ret = self.recv_endswith("\r\n").strip()
+        if ret[0] == '5':
+            self.save_mail(self.server_user, sender, self.create_mail(sender, ret))
+            return
 
         tcpclient.close()
-        self.request.sendall(b'250 queued as\r\n')
+
+    def solve_work(self, work):
+        for rec in work[1]:
+            if rec.endswith(self.server_domain):
+                self.save_mail(work[0], rec, work[2])
+            else:
+                self.send_mail(work[0], rec, work[2])
 
     def handle(self):
 
+        self.domain = ""
         self.server_domain = "msc.com"
+        self.server_user = "manager@msc.com"
         self.request.sendall(b'220 welcome ,i"m here\r\n')
         self.client_ip = self.client_address[0]
         error_msg = "502 Invalid input from " + self.client_ip + " to msc.com\r\n"
+        noop_msg = "250 OK from " + self.client_ip + "to msc.com\r\n"
 
         self.process_turn = 0
-        self.client_type = 2
+        self.client_type = 0
+
+        self.total_works = []
+        self.one_work = []
 
         while True:
             self.data = self.recv_endswith("\r\n").strip()
             if self.data.upper() == "QUIT":
-                self.request.sendall(b'221 bye\r\n')
-                break
+                for work in self.total_works:
+                    self.solve_work(work)
+                self.request.sendall(b'221 Bye\r\n')
+                return
+
+            elif self.data.upper() == "NOOP":
+                self.request.sendall(noop_msg.encode())
+
+            elif self.data.upper() == "RSET":
+                self.total_works = ()
+                self.process_turn = 0
+                self.client_type = 0
+                self.request.sendall(b'250 OK\r\n')
 
             elif self.data[0:4].upper() == "EHLO":
                 self.domain = self.data[5:]
-
                 self.check_ip_list()
-
-                self.request.sendall(b'''250-msc.com
-250-SIZE 73400320
-250-AUTH LOGIN 
-250-AUTH=LOGIN
-250 8BITMIME\r\n.\r\n''')
 
             elif self.data.upper() == "AUTH LOGIN":
 
                 self.request.sendall(b'334 VXNlcm5hbWU6\r\n')
                 self.data = self.recv_endswith("\r\n").strip()
                 self.client_name = base64.b64decode(self.data).decode("utf-8")
+                self.client_name = self.client_name.replace("@msc.com", "")
 
                 self.request.sendall(b'334 UGFzc3dvcmQ6\r\n')
                 self.data = self.recv_endswith("\r\n").strip()
@@ -169,10 +243,11 @@ class Myserver(socketserver.BaseRequestHandler):
                 if self.client_type == 2:
                     self.request.sendall(b'235 Authentication successful\r\n')
                 else:
-                    self.request.sendall(b'502 unable to fetch data\r\n')
+                    self.request.sendall(b'535 Login Fail. Please enter right information to login.\r\n')
 
             elif (self.data[0:11] + self.data[-1]).upper() == "MAIL FROM:<>":
                 if self.client_type != 0:
+                    self.one_work = []
                     self.sender_addr = self.data[11:-1]
                     self.receiver_list = []
                     self.request.sendall(b'250 OK\r\n')
@@ -182,14 +257,12 @@ class Myserver(socketserver.BaseRequestHandler):
 
             elif (self.data[0:9] + self.data[-1]).upper() == "RCPT TO:<>":
                 if self.process_turn >= 1:
-                    print(self.data[9:-1])
-                    print(self.data[9:-2])
                     if self.check_existence(self.data[9:-1]):
                         self.receiver_list.append(self.data[9:-1])
                         self.request.sendall(b'250 OK\r\n')
                         self.process_turn = 2
                     else:
-                        self.request.sendall(b'502 unable to fetch data\r\n')
+                        self.request.sendall(b'551 User not exist; please try again')
                 else:
                     self.request.sendall(b'503 Send command mailfrom first.\r\n')
 
@@ -198,12 +271,12 @@ class Myserver(socketserver.BaseRequestHandler):
                     self.request.sendall(b'354 End data with <CR><LF>.<CR><LF>.\r\n')
                     self.content = self.recv_endswith("\r\n.\r\n").strip()
                     if len(self.content) <= 73400320:
-                        for rec in self.receiver_list:
-                            if rec.endswith(self.server_domain):
-                                self.save_mail(rec)
-                            else:
-                                self.send_mail(rec)
+                        self.one_work.append(self.sender_addr)
+                        self.one_work.append(self.receiver_list)
+                        self.one_work.append(self.content)
+                        self.total_works.append(self.one_work)
                         self.process_turn = 0
+                        self.request.sendall(b'250 OK:queued as.\r\n')
                     else:
                         self.request.sendall(b'503 Content is too long.\r\n')
                 else:
